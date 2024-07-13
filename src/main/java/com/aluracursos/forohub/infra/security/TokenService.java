d0cc57466e699796fd1820a656356629e045bb4e
Pineda Galindo Ricardo Angel <ricki.pg3@gmail.com>
2024-07-12 23:12:20 -0600

    Implementación de la autenticación para cada petición que se realice

--- src/main/java/com/aluracursos/forohub/infra/security/TokenService.java
+++ src/main/java/com/aluracursos/forohub/infra/security/TokenService.java
@@ -4,6 +4,8 @@ import com.aluracursos.forohub.domain.modelos.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -32,5 +34,26 @@ public class TokenService {
 
     private Instant generarFechaExpiracion(long tiempoExpiracion, String zonaHoraria){
         return LocalDateTime.now().plusHours(tiempoExpiracion).toInstant(ZoneOffset.of(zonaHoraria));
+    }
+
+    public String getSubject(String token) {
+        if (token == null) {
+            throw  new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(api_secret);
+            verifier = JWT.require(algorithm)
+                    // specify any specific claim validations
+                    .withIssuer("foro_hub")
+                    .build()
+                    .verify(token);
+        } catch (JWTVerificationException exception){
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null) {
+            throw new RuntimeException("Verifier inválido");
+        }
+        return verifier.getSubject();
     }
 }
